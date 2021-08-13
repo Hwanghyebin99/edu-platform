7550d07ced5dc1cf9c682f6ca0a2f183bdc4e046
황혜빈 <hebin99@naver.com>
2021-08-13 20:22:06 +0900

    add Community Component

---
+++ src/components/contents/components/Community/Community.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Community extends Component {
+  render() {
+    return (
+      <div className="main-section">
+          community
+      </div>
+    );
+  }
+}
+
+export default Community;
