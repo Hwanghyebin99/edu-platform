da57a2b779cd4edbda205ff309f4d66d862037e3
황혜빈 <hebin99@naver.com>
2021-08-13 20:26:02 +0900

    add Sidebar Component

---
+++ src/components/shared/components/side-bar/Sidebar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <>
+        <div className="sidebar-container">
+            <ul>
+                <li>
+                    content
+                </li>
+                <li>
+                    content
+                </li>
+            </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Sidebar;
